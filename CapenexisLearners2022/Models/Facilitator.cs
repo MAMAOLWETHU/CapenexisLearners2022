bf16edf9b7ba8b03ae51bc8f33b52598297ed008
MAMAOLWETHU <thulisiweb04@gmail.com>
2023-01-18 08:43:18 +0200

    Add project files.

---
+++ CapenexisLearners2022/Models/Facilitator.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapenexisLearners2022.Models;
+
+public class Facilitator
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? NationalIdentityNumber { get; set; }
+    public string? NumberofLearners { get; set; }
+}
---
+++ CapenexisLearners2022/Models/Learner.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapenexisLearners2022.Models;
+
+public class Learner
+{
+    public int Id { get; set; }
+    public string? FirstName{ get; set; }
+    public string? LastName { get; set; }
+    public string? NationalIdentityNumber { get; set; }
+    public string? Course { get; set; }
+    public decimal Price { get; set; }
+}
